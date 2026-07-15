3d282bbc7d41794d48c5fd1bc1c946fa891d8363
Tharun-m19 <tharunmadhaiyan07@gmail.com>
2026-07-15 14:20:57 +0530

    initial commit - tharun

---
+++ Lab2/addition.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a=0;
+  int b=0;
+  cout<<"Enter num 1 : ";
+  cin>>a;
+  cout<<"Enter num 2 : ";
+  cin>>b;
+  cout<<"Multiplication result : "<<a*b<<endl;
+}
